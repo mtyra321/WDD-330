b788c2552f20ad9a3ad56810223e434eddb029a3
mtyra321 <mtyra321@gmail.com>
2020-09-17 19:35:35 -0600

    add table of contents done

--- table_of_contents.js
+++ table_of_contents.js
@@ -1,6 +1,23 @@
 function poplist() {
-    document.getElementById
-        //link js to html textbox
-        //do loop
-        //add array
+    var list = document.getElementById("list");
+    var arraylink = ["Week 1\\form.html"];
+    var arrayname = ["week one form example"];
+
+    for (let index = 0; index < arraylink.length; index++) {
+        const element = arraylink[index];
+        const name = arrayname[index];
+        var li = document.createElement("li");
+        //var a creates an A element
+        var a = document.createElement("a");
+        //set a to a href and give it the site link
+        a.setAttribute('href', element);
+        //make a display the name
+        a.innerText = name;
+        console.log(element);
+        li.appendChild(a);
+
+        console.log(li);
+        list.appendChild(li);
+
+    }
 }
