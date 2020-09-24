d946ec1be940edd52d01711752ec84cfbe3c7327
mtyra321 <mtyra321@gmail.com>
2020-09-24 11:06:05 -0600

    did team activty

--- Week 2/teamAct.js
+++ Week 2/teamAct.js
@@ -6,3 +6,45 @@ function displayInfo() {
     div.innerText = info;
 
 }
+
+function addUpTo(num) {
+    let sum = 0;
+    const div = document.getElementById("results2");
+    for (let index = 0; index <= num; index++) {
+        sum += index;
+    }
+    div.innerText = sum;
+
+}
+
+function add() {
+    let div = document.getElementById("results3");
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    div.innerText = parseInt(num1) + parseInt(num2);
+}
+
+function subtract() {
+    let div = document.getElementById("results3");
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    div.innerText = parseInt(num1) - parseInt(num2);
+}
+
+function mult() {
+    let div = document.getElementById("results3");
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    div.innerText = parseInt(num1) * parseInt(num2);
+}
+
+function division() {
+    let div = document.getElementById("results3");
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    div.innerText = parseInt(num1) / parseInt(num2);
+}
