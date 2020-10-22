6428a744fc019a6427373a9634b4d69cd6c63965
mtyra321 <mtyra321@gmail.com>
2020-10-22 15:55:51 -0600

    todo saves and does flex

--- Todo/todo.js
+++ Todo/todo.js
@@ -31,15 +31,21 @@ class Todo {
     completeTodo(todo) {
 
     }
-    deleteTodo() {
-
+    z() {
+        console.log("wast");
+        document.getElementById("label").innerText = "test";
     }
 
     clearTodos() {
 
     }
-
+    a() {
+        console.log("check");
+        return false;
+    }
     displayAllTodos(key) {
+
+
         todos = getFromLS(key);
         console.log("Length " + todos.length);
         let list = qs('#todoList');
@@ -49,31 +55,74 @@ class Todo {
             //grabbing the text from the todo object
             let text = document.createElement("p");
             text.textContent = element.text;
-            //creating a bullet object for the list
-            let bullet = document.createElement("li");
+            text.class = "text";
             //checkmark button
-            let checkButt = document.createElement("button");
-            checkButt.textContent = "  ";
-
-
+            let checkButt = this.configureCheckButt(element);
             //delete button
-            let deleteButt = document.createElement("button");
-            deleteButt.textContent = "X";
+            let deleteButt = this.configureDeleteButt(element);
+
+            //   deleteButt.onclick = function() { this.z(); };
+            console.log(deleteButt.onclick);
+            // deleteButt.onclick = function() {
+            //     console.log("asdfhj");
+            // };
             //div to hold everything
-            let div = document.createElement("div");
+            let div = this.configureDiv();
             //adding the check button, text, and delete button to the div
-            div.appendChild(checkButt);
-            div.appendChild(text);
-            div.appendChild(deleteButt);
+            // div.appendChild(checkButt);
+            // div.appendChild(text);
+            // div.appendChild(deleteButt);
+            div.innerHTML = checkButt.outerHTML + text.outerHTML + deleteButt.outerHTML;
+
             //adding the div to the bullet and the bullet to the list
-            bullet.innerHTML = div.innerHTML;
-            list.appendChild(bullet);
+            list.appendChild(div);
         }
 
     }
 
+    configureDiv() {
+        let div = document.createElement("div");
+        div.classList.add('e');
+        // div.class = "flex-container";
+        div.display = "flex";
+        // div.flexdDirection = "row";
+        // div.height = 200;
+        // div.alignItems = "flex-start";
+        // div.backgroundColor = "DodgerBlue";
+        return div;
+    }
 
 
+    configureDeleteButt(element) {
+        let deleteButt = document.createElement("button");
+        deleteButt.textContent = "Delete";
+        deleteButt.classList.add('deleteButt');
+
+        deleteButt.display = "inline-flex";
+        deleteButt.setAttribute('onClick', 'this.z');
+
+        deleteButt.class = "deleteButt";
+        return deleteButt;
+    }
+    configureCheckButt(element) {
+        let checkButt = document.createElement("button");
+        checkButt.textContent = "Check Off";
+        checkButt.display = "inline-flex";
+        checkButt.classList.add('checkButt');
+        checkButt.setAttribute('onClick', 'this.a');
+
+        checkButt.class = "checkButt";
+        return checkButt;
+    }
+
+
+
+
+}
+
+
+function tester() {
+    console.log("test");
 }
 
 export default Todo;
