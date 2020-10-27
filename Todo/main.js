f795553f3b3333655e0c9256c9ab43553dff155c
mtyra321 <mtyra321@gmail.com>
2020-10-27 13:39:48 -0600

    finished todo

--- Todo/main.js
+++ Todo/main.js
@@ -26,22 +26,142 @@ document.getElementById("Task").addEventListener("keydown", function(e) {
 //qs('#addButton').addEventListener('touchend', addButton);
 let Button = qs('#addButton');
 Button.addEventListener('click', addButton);
-qs('body').addEventListener('onload', myTodo.displayAllTodos(myTodo.key));
+qs('body').addEventListener('onload', displayAllTodos(myTodo.key));
 qs('#All').addEventListener('click', function() {
-    myTodo.displayAllTodos(myTodo.key);
+    displayAllTodos(myTodo.key);
 });
-qs('#Completed').addEventListener('click', myTodo.displayCompletedTodos);
-qs('#Active').addEventListener('click', myTodo.displayActiveTodos);
+qs('#Completed').addEventListener('click', function() { displayCompletedTodos(myTodo.key) });
+qs('#Active').addEventListener('click', function() {
+    displayActiveTodos(myTodo.key)
+});
 
 function addButton() {
-    console.log("add");
     const inputElement = qs('#Task').value;
 
-    console.log("value is " + inputElement);
     myTodo.addNewTodo(inputElement, myTodo.key);
-    myTodo.displayAllTodos(myTodo.key);
+    displayAllTodos(myTodo.key);
     qs('#Task').value = "";
 }
+
+function displayAllTodos(key) {
+    myTodo.todos = myTodo.grabTodos(key);
+
+    let list = qs('#todoList');
+    list.innerHTML = "";
+    for (let index = 0; index < myTodo.todos.length; index++) {
+        let element = myTodo.todos[index];
+        //grabbing the text from the todo object
+        let div = createListItem(myTodo.todos[index], index);
+        list.appendChild(div);
+
+    }
+}
+
+function displayActiveTodos(key) {
+    myTodo.todos = myTodo.loadTodos(key);
+
+    console.log("active");
+    let list = qs('#todoList');
+    list.innerHTML = "";
+    for (let index = 0; index < myTodo.todos.length; index++) {
+        const element = myTodo.todos[index];
+        if (element.completed == false) {
+
+            let div = createListItem(element, index);
+            list.appendChild(div);
+        }
+
+    }
+
+}
+
+function createListItem(element, index) {
+    let text = document.createElement("p");
+    text.textContent = element.text;
+    text.class = "text";
+    if (element.completed == true) {
+        text.textDecoration = "line-through";
+    }
+    //checkmark button
+    let checkButt = configureCheckButt(element, index);
+    //delete button
+    let deleteButt = configureDeleteButt(element, index);
+    //div to hold everything
+    let div = configureDiv(index);
+    //adding the check button, text, and delete button to the div
+    div.appendChild(checkButt);
+    div.appendChild(text);
+    div.appendChild(deleteButt);
+
+    return div;
+
+}
+
+function displayCompletedTodos(key) {
+    myTodo.todos = myTodo.loadTodos(key);
+
+    let list = qs('#todoList');
+    list.innerHTML = "";
+    for (let index = 0; index < myTodo.todos.length; index++) {
+        const element = myTodo.todos[index];
+        if (element.completed == true) {
+            let div = createListItem(element, index);
+            list.appendChild(div);
+
+        }
+
+    }
+}
+
+function configureDiv(index) {
+    let div = document.createElement("div");
+    div.classList.add('e');
+    div.id = "div" + index;
+    return div;
+}
+
+
+function configureDeleteButt(element, index) {
+    var deleteButt = document.createElement("button");
+    deleteButt.textContent = "Delete";
+    deleteButt.classList.add('deleteButt');
+    deleteButt.addEventListener('click', function() {
+        myTodo.deleteTodo(element, index);
+    });
+
+    return deleteButt;
+}
+
+function configureCheckButt(element, index) {
+
+    var checkButt = document.createElement("button");
+    checkButt.textContent = "Check Off";
+    checkButt.classList.add('checkButt');
+    console.log(element.completed);
+    if (element.completed == true) {
+        checkButt.style.backgroundColor = "#7FFF00";
+        checkButt.textContent = "Complete";
+
+    } else {
+        checkButt.style.backgroundColor = "#FF0000";
+        checkButt.textContent = "Check Off";
+
+    }
+    checkButt.addEventListener('click', function() {
+        let complete = myTodo.completeTodo(element);
+        if (complete == true) {
+            checkButt.style.backgroundColor = "#7FFF00";
+            checkButt.textContent = "Complete";
+        } else {
+            checkButt.style.backgroundColor = "#FF0000";
+            checkButt.textContent = "Check Off";
+
+        }
+    });
+    return checkButt;
+}
+
+
 
 
 import Todo from "./todo.js";
--- Todo/todo.js
+++ Todo/todo.js
@@ -3,158 +3,81 @@ import { qs, saveToLS, getFromLS } from './utilities.js';
 var todos;
 
 function saveTodos(key) {
+    console.log("saving");
     saveToLS(key, todos);
+}
+
+function loadTodos(key) {
+    console.log("loading");
+    return getFromLS(key);
 }
 class Todo {
     constructor(parentId, key) {
         this.listElement = qs(parentId);
         this.key = key;
         //  this.displayAllTodos(todos);
-        todos = [];
+        todos = getFromLS(key);
     }
 
 
-    addNewTodo(text) {
+    addNewTodo(text, key) {
         const newTodo = {
             id: new Date(),
             text: text,
             completed: false
         }
+        newTodo.id = newTodo.id.toString();
         todos.push(newTodo);
-
-        saveTodos(this.key);
+        saveTodos(key);
     }
-
-    getText() {
-        return this.text;
+    grabTodos(key) {
+        return loadTodos(key);
     }
 
     completeTodo(todo) {
+        let complete = false;
+        for (let index = 0; index < todos.length; index++) {
+            const element = todos[index];
+            if (todos[index].id == todo.id) {
+                if (todos[index].completed == false) {
+                    todos[index].completed = true;
+                    console.log("changed to true");
+                    todos[index].text.strike();
+                    complete = true;
 
+                } else {
+                    todos[index].completed = false;
+
+                }
+
+            }
+
+
+        }
+
+        saveTodos(this.key);
+        loadTodos(this.key);
+        return complete;
     }
 
-    z() {
-        console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
-    }
+
 
     clearTodos() {
 
     }
-    a() {
-        console.log("aaaaaaaaaaaaaaaaaaaaa");
-        return false;
+
+    deleteTodo(element, index) {
+        let div = document.getElementById("div" + index);
+        div.remove();
+        todos.splice(index, 1);
+        saveTodos(this.key);
     }
-    displayAllTodos(key) {
-        console.log("inside display all");
-        todos = getFromLS(key);
-
-        console.log("Length " + todos.length);
-        let list = qs('#todoList');
-        list.innerHTML = "";
-        for (let index = 0; index < todos.length; index++) {
-            let element = todos[index];
-            //grabbing the text from the todo object
-            let div = this.createListItem(element);
-            list.appendChild(div);
-
-        }
-
-    }
-    displayActiveTodos() {
-
-        console.log("active");
-        let list = qs('#todoList');
-        list.innerHTML = "";
-        for (let index = 0; index < todos.length; index++) {
-            const element = todos[index];
-            console.log(element.completed);
-            if (element.completed == false) {
-                console.log("in if");
-
-                let div = this.createListItem(element);
-
-                list.appendChild(div);
-            }
-
-        }
-
-    }
-
-    displayCompletedTodos() {
-
-        console.log("complete");
-        let list = qs('#todoList');
-        list.innerHTML = "";
-        for (let index = 0; index < todos.length; index++) {
-            const element = todos[index];
-            if (element.completed == true) {
-                let div = this.createListItem(element);
-                list.appendChild(div);
-
-            }
-
-        }
-    }
-    createListItem(element) {
-        console.log("Inside createListItem");
-        let text = document.createElement("p");
-        text.textContent = element.text;
-        text.class = "text";
-        //checkmark button
-        let checkButt = this.configureCheckButt(element);
-        //delete button
-        let deleteButt = this.configureDeleteButt(element);
-        deleteButt.addEventListener('click', function() {
-            z();
-        });
-        //div to hold everything
-        let div = this.configureDiv();
-        //adding the check button, text, and delete button to the div
-
-        div.innerHTML = checkButt.outerHTML + text.outerHTML + deleteButt.outerHTML;
-        return div;
-
-    }
-
-
-
-    configureDiv() {
-        let div = document.createElement("div");
-        div.classList.add('e');
-        return div;
-    }
-
-
-    configureDeleteButt(element) {
-        var deleteButt = document.createElement("button");
-        deleteButt.textContent = "Delete";
-        deleteButt.classList.add('deleteButt');
-        console.log("COnfigure butt");
-        deleteButt.addEventListener('click', Todo.z);
-
-        return deleteButt;
-    }
-    configureCheckButt(element) {
-        var checkButt = document.createElement("button");
-        checkButt.textContent = "Check Off";
-        checkButt.classList.add('checkButt');
-
-        return checkButt;
-    }
-
 
 
 
 }
 
 
-function tester(deleteButt) {
-    deleteButt.addEventListener('click', w);
-    console.log("test");
-    return deleteButt;
-}
 
-function w() {
-    console.log("w");
-}
+
 export default Todo;
